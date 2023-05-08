1f6c1abc24a72440f0633b1ee2dd7e8725b02c2a
AlbertoScalia <albscalia@gmail.com>
2023-05-08 15:12:32 +0200

    Feat (JS) : Add methods

--- script.js
+++ script.js
@@ -4,7 +4,16 @@ createApp({
     data() {
         return {
             messaggio: 'Hello world from',
-            logo: 'https://www.vectorlogo.zone/logos/vuejs/vuejs-icon.svg'
+            logo: 'https://www.vectorlogo.zone/logos/vuejs/vuejs-icon.svg',
+            is_colore_secondario: false
+        }
+    },
+
+    methods: {
+        cambioColoreSfondo() {
+            console.log('ciao')
+            this.is_colore_secondario = !this.is_colore_secondario; 
+            console.log(this.is_colore_secondario)
         }
     }
 }).mount('#app');
